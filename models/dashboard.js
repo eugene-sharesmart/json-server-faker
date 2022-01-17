d837a624993ffb857a31e0a1ae200727d221b4b8
Eugene <95874084+eugene-sharesmart@users.noreply.github.com>
2022-01-17 14:39:00 -0800

    static id for dashboard

--- models/dashboard.js
+++ models/dashboard.js
@@ -1,16 +1,13 @@
 import faker from "@faker-js/faker";
-import { v1 as uuid } from "uuid";
 
 faker.locale = 'en_CA'
 
 export const GetDashboard = () => {
   const ts = Date.now()
   return {
-    id: uuid()
+    id: 1
     , msgSent: faker.datatype.number(20)
     , msgTotal: faker.datatype.number(300)
     , usersOnline: faker.datatype.number(10000)
-    , createdAt: ts
-    , updatedAt: ts
   }
 }
